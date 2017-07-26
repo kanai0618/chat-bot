b963641a3b4c50fdcf5af5f83c0820ac4325b346
Satyajit Bhowmik <satyajit_bhowmik@intuit.com>
2017-07-26 21:56:06 +0530

    Adding webhook call

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+//newly added route
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "my-chat-bot") {
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    res.sendStatus(403);
+  }
 });
 app.listen(port, function () {
   console.log('Example app listening on port: '+ port)
